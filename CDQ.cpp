539fb08bf5ecf954e73d4982628b4f02dd9ee8ef
mk13579 <32061701+mk13579@users.noreply.github.com>
2020-06-27 15:19:12 +0800

    Update CDQ.cpp

--- CDQ.cpp
+++ CDQ.cpp
@@ -1,65 +1,92 @@
-#include<cstdio>
-#include<cstring>
-#include<iostream>
-#include<algorithm>
-#define maxn 100010
+#include<bits/stdc++.h>
 using namespace std;
-int n,k,ans[maxn]={0},d[maxn]={0};
+const int N = 10005;
 struct node{
-    int x,y,z;
-    bool b;
-    int *ans;
-    inline void get(){
-        scanf("%d%d%d",&x,&y,&z);
-        return;
-    }
-    bool operator==(const node &a) const{
-        return x==a.x&&y==a.y&&z==a.z;
-    }
-}a[maxn],b[maxn],c[maxn];
-inline bool cmp(const node &a, const node &b){
-    return a.x<b.x||(a.x==b.x&&a.y<b.y)||(a.x==b.x&&a.y==b.y&&a.z<b.z);
+	int id;
+	int v;
+	bool mk;
+	int x,y,z;
+	node(int _x=0,int _y=0,int _z=0){
+		x=_x;y=_y;z=_z;
+		mk=0;
+	}
+	bool operator == (node a){
+		return x==a.x&&y==a.y&&z==a.z;
+	}
+}a[N],b[N],c[N];
+int n=0;
+bool cmp(node a,node b){
+	if(a.x==b.x){
+		if(a.y==b.y) return a.z<b.z;
+		return a.y<b.y;
+	}
+	return a.x<b.x;
 }
-void merge2(int l,int r){
-    if(l==r)return;
-    int mid=(l+r)>>1;
-    merge2(l,mid);
-    merge2(mid+1,r);
-    for(int i=l,j=l,k=mid+1,cnt=0;i<=r;++i){
-        if((k>r||b[j].z<=b[k].z)&&j<=mid) c[i]=b[j++],cnt+=c[i].b;
-        else{
-            c[i]=b[k++];
-            if(!c[i].b)*c[i].ans+=cnt;
-        }
-    }
-    for(int i=l;i<=r;++i)b[i]=c[i];
+bool cmp1(node a,node b){
+	return a.id<b.id;
 }
-void merge1(int l,int r){
-    if(l==r)return;
-    int mid=(l+r)>>1;
-    merge1(l,mid);
-    merge1(mid+1,r);
-    for(int i=l,j=l,k=mid+1;i<=r;++i){
-        if((k>r||a[j].y<=a[k].y)&&j<=mid) b[i]=a[j++],b[i].b=1;
-        else b[i]=a[k++],b[i].b=0;
-    }
-    for(int i=l;i<=r;++i)a[i]=b[i];
-    merge2(l,r);
+void merge2(node *b,int l,int r){
+	if(l==r) return;
+	int mid=(l+r)>>1;
+	merge2(b,l,mid);
+	merge2(b,mid+1,r);
+	int i=l,j=mid+1;
+	int t=l;
+	int sum=0;
+	while(i<=mid&&j<=r){
+		if(b[i].z<=b[j].z){
+			c[t]=b[i++];sum+=c[t].mk;t++;
+		}else{
+			c[t]=b[j++];c[t].v+=sum;t++;
+		}
+	}
+	while(i<=mid){
+		c[t]=b[i++];sum+=c[t].mk;t++;
+	}
+	while(j<=r){
+		c[t]=b[j++];c[t].v+=sum;t++;
+	}
+	for(int i=l;i<=r;i++) b[i]=c[i];
+}
+void merge1(node *a,int l,int r){
+	if(l==r) return;
+	int mid=(l+r)>>1;
+	merge1(a,l,mid);
+	merge1(a,mid+1,r);
+	int i=l,j=mid+1;
+	int t=l;
+	while(i<=mid&&j<=r){
+		if(a[i].y<=a[j].y){
+			b[t]=a[i++];b[t].mk=1;t++;	
+		}else{
+			b[t]=a[j++];b[t].mk=0;t++;
+		}
+	}
+	while(i<=mid){
+		b[t]=a[i++];b[t].mk=1;t++;
+	}
+	while(j<=r){
+		b[t]=a[j++];b[t].mk=0;t++;
+	}
+	for(int i=l;i<=r;i++) a[i]=b[i];
+	merge2(b,l,r);
 }
 int main(){
-    scanf("%d%d",&n,&k);
-    for(int i=1;i<=n;++i) a[i].get(),a[i].ans=&ans[i],ans[i]=0;
-    sort(a+1,a+n+1,cmp);
-    for(int i=n-1;i;--i) if(a[i]==a[i+1]) *a[i].ans=*a[i+1].ans+1;
-    merge1(1,n);
-    for(int i=1;i<=n;++i) ++d[ans[i]];
-    for(int i=0;i<n;++i) printf("%d\n",d[i]);
-    return 0;
-}
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		cin>>a[i].x>>a[i].y>>a[i].z;a[i].v=0;a[i].id=i;
+	}
+	sort(a+1,a+1+n,cmp);
+	for(int i=n-1;i>=1;i--) if(a[i]==a[i+1]) a[i].v=a[i+1].v+1;
+	merge1(a,1,n);
+	sort(c+1,c+1+n,cmp1);
+	for(int i=1;i<=n;i++) cout<<c[i].v<<endl;
+	return 0;
+} 
+
 /*
 
+三维偏序的CDQ 
 
-CDQ分治
 
-
-*/ 
+*/
